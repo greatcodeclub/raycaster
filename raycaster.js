dae4994ac2bc1c282f4907735690cfae6874804f
macournoyer <macournoyer@gmail.com>
2014-06-30 22:28:55 -0400

    Prep code for live coding.

--- raycaster.js
+++ raycaster.js
@@ -3,32 +3,6 @@ var DEG = Math.PI / 180 // 1 deg == pi/180 radian
 var canvas = document.getElementById("screen")
 var map = new Map()
 var camera = new Camera(map)
-var game = new Game(canvas)
 
-game.onFrame(function() {
-  // Move the camera
-  if (game.keyPressed.up) {
-    camera.move(10)
-  } else if (game.keyPressed.down) {
-    camera.move(-10)
-  }
-
-  // Rotate the camera
-  if (game.keyPressed.left) {
-    camera.angle -= 1
-  } else if (game.keyPressed.right) {
-    camera.angle += 1
-  }
-
-  var context = canvas.getContext("2d")
-
-  // Clear the screen
-  context.fillStyle = '#fff'
-  context.fillRect(0, 0, canvas.width, canvas.height)
-
-  // Project the camera on the canvas
-  camera.project(map, canvas)
-
-  // Draw the mini-map
-  map.draw(canvas, camera)
-})
+// Draw the mini-map
+map.draw(canvas, camera)
