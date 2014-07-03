ae97a96ff1a5cff240e3529268280ba5e3411bb2
macournoyer <macournoyer@gmail.com>
2014-07-03 09:56:09 -0400

    Add game engine.

---
+++ game.js
@@ -0,0 +1,51 @@
+function Game(canvas) {
+  var self = this
+
+  // Keep track of key states
+  // Eg.:
+  //   game.keyPressed.up === true  // while UP key is pressed)
+  //   game.keyPressed.up === false // when UP key is released)
+  this.keyPressed = {}
+
+  $(canvas).on('keydown keyup', function(e) {
+    // Convert key code to key name
+    var keyName = Game.keys[e.which]
+
+    if (keyName) {
+      // eg.: `self.keyPressed.up = true` on keydown
+      // Will be set to `false` on keyup
+      self.keyPressed[keyName] = e.type === 'keydown'
+      e.preventDefault()
+    }
+  })
+}
+
+// Some key code to key name mappings
+Game.keys = {
+  32: 'space',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down'
+}
+
+// Setup the game loop.
+//
+// Calls the `callback` each time the screen needs to be redrawn.
+Game.prototype.onFrame = function(callback) {
+  var self = this
+
+  if (window.requestAnimationFrame) {
+    requestAnimationFrame(function() {
+      callback()
+      // requestAnimationFrame only calls our callback once, we need to
+      // schedule the next call ourself.
+      self.onFrame(callback)
+    })
+  } else {
+    // requestAnimationFrame is not supported by all browsers. We fall back to
+    // a timer.
+    var fps = 60
+    setInterval(callback, 1000 / fps)
+  }
+}
--- raycaster.js
+++ raycaster.js
@@ -3,6 +3,7 @@ var DEG = Math.PI / 180 // 1 deg == pi/180 radian
 var canvas = document.getElementById("screen")
 var map = new Map()
 var camera = new Camera(map)
+var game = new Game(canvas)
 
 camera.project(map, canvas)
 
